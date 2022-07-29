1088177e2eb0fefada595be3e4f9cd71b1a56743
Nathan Burgess <nathans.email.address@gmail.com>
2022-07-28 17:37:57 -0700

    MVP

--- app.js
+++ app.js
@@ -7,34 +7,54 @@ const monumentImgEl = document.getElementById("monument-img");
 const sloganInputEl = document.getElementById("slogan-input");
 const sloganButtonEl = document.getElementById("slogan-button");
 const sloganListEl = document.getElementById("slogans");
+const countersEl = document.getElementById("counters");
 
 let slogans = [];
+let skylineCounter = 0;
+let natureCounter = 0;
+let monumentCounter = 0;
 
 
 skylineDropboxEl.addEventListener('change', () => {
+    skylineCounter++;
     const choice = skylineDropboxEl.value;
     skylineImgEl.src = `assets/${choice}.png`;
+    displayCountStats();
 })
 
 natureDropboxEl.addEventListener('change', () => {
+    natureCounter++;
     const choice = natureDropboxEl.value;
     natureImgEl.src = `assets/${choice}.png`;
+    displayCountStats();
+
+    
 })
 
 monumentDropboxEl.addEventListener('change', () => {
+    monumentCounter++;
     const choice = monumentDropboxEl.value;
     monumentImgEl.src = `assets/${choice}.png`;
+    displayCountStats();
+
 })
 
 sloganButtonEl.addEventListener('click', () => {
     const sloganValue = sloganInputEl.value;
     slogans.push(sloganValue);
+    displayCatchphrases();
+})
+
+function displayCountStats() {
+    countersEl.textContent = `You changed Skyline ${skylineCounter} times, Nature ${natureCounter} times, and Monuments ${monumentCounter} times.`
+}
+
+function displayCatchphrases() {
     sloganListEl.textContent = "";
+    sloganInputEl.value = "";
     for (let slogan of slogans) {
         const sloganListItemEl = document.createElement('li');
         sloganListItemEl.textContent = slogan;
         sloganListEl.appendChild(sloganListItemEl);
     }
-
-
-})
+}
